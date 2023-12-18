a4779f4ebd091910442786f8173a86ee7139b233
Sahil Mate <143534628+sahilmate@users.noreply.github.com>
2023-12-18 06:21:22 +0000

    set homepage for github pages deploy

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "Sahil Mate",
+  title: "Full Stack Developer / Student",
+  email: "matesahil04@gmail.com",
+  gitHub: "sahilmate",
+  instagram: "sahilmate_",
+  linkedIn: "sahilmate",
+  medium: "matesahil04",
+  twitter: "sahiilmate",
+  youTube: "sahiilmate",
 };
 
 const primaryColor = "#4E567E";
